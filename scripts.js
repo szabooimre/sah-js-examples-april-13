cc2e429f3068ad893472f570001f00fb53b74c8a
szabooimre <33134351+szabooimre@users.noreply.github.com>
2020-04-23 17:03:54 +0200

    first couple trials

---
+++ scripts.js
@@ -0,0 +1,7 @@
+let firstname = "Imre";
+console.log(firstname + "'s apples");
+console.log("Hello World!")
+/* general selector
+$('selector').action()
+*/
+$('h1').text('We are having a 10 minute break until 17:05')
